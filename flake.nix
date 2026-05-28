e499b952b724b4f12c35d0e3072f43c5e5fc27fb
moons-14 <moons@moons14.com>
2026-05-28 10:37:04 +0900

    chore: add nixConfig with cachix substituters and trusted public keys

--- flake.nix
+++ flake.nix
@@ -71,6 +71,20 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://moons-dotfiles.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "moons-dotfiles.cachix.org-1:WHoroKiNScG2/dpxHHL1I0qVmvuQhJbEAP+DS2j9Rr0="
+    ];
+  };
+
   outputs =
     inputs@{
       flake-parts,
