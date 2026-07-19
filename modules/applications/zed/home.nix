6ec1f5762ac33f2d8b6995904693444e44c672c2
moons <moons@moons14.com>
2026-07-20 07:10:25 +0900

    imutable zed settings

--- modules/applications/zed/home.nix
+++ modules/applications/zed/home.nix
@@ -14,7 +14,48 @@ in
   config.home-manager.sharedModules = [
     {
       config = lib.mkIf cfg.enable {
-        programs.zed-editor.enable = true;
+        programs.zed-editor = {
+          enable = true;
+          mutableUserSettings = false;
+
+          userSettings = {
+            agent = {
+              flexible = true;
+              favorite_models = [ ];
+              model_parameters = [ ];
+            };
+
+            project_panel.dock = "left";
+
+            telemetry = {
+              diagnostics = false;
+              metrics = false;
+            };
+
+            vim_mode = true;
+
+            agent_servers = {
+              github-copilot-cli.type = "registry";
+              codex-acp.type = "registry";
+              claude-acp.type = "registry";
+            };
+
+            icon_theme = {
+              mode = "dark";
+              light = "Zed (Default)";
+              dark = "Zed (Default)";
+            };
+
+            ui_font_size = 18;
+            buffer_font_size = 20;
+
+            theme = {
+              mode = "dark";
+              light = "Dracula";
+              dark = "Dracula";
+            };
+          };
+        };
       };
     }
   ];
