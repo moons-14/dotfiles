a8113633dbf5868329ac46e1afc9b3c775b53f46
moons <moons@moons14.com>
2026-07-27 21:29:20 +0900

    oxker test

--- modules/applications/docker/home.nix
+++ modules/applications/docker/home.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }:
+let
+  oxker = pkgs.oxker.overrideAttrs (oldAttrs: {
+    checkFlags =
+      (oldAttrs.checkFlags or [ ])
+      ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+        "--skip=ui::draw_blocks::help::tests::test_draw_blocks_help_custom_keymap_one_two_definition"
+        "--skip=ui::draw_blocks::help::tests::test_draw_blocks_help_custom_keymap_two_definition"
+      ];
+  });
+in
 {
   home.packages = [
     pkgs.docker-client
-    pkgs.oxker
+    oxker
   ];
 }
