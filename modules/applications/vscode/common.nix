9061825c6314c60898bc9dd7076371b2f6dd867c
moons <moons@moons14.com>
2026-07-27 16:10:03 +0900

    add apps

--- modules/applications/vscode/common.nix
+++
@@ -1,5 +0,0 @@
-{
-  # Home Manager uses the system package set on both NixOS and nix-darwin.
-  # VS Code itself is unfree, so permit it where this application is selected.
-  nixpkgs.config.allowUnfree = true;
-}
