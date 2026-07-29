c566f426ece5ebe8de3a6a7813d7fbcbc3622c4f
moons <moons@moons14.com>
2026-07-30 02:05:35 +0900

    zoom

--- modules/applications/zoom/home.nix
+++ modules/applications/zoom/home.nix
@@ -1,4 +1,10 @@
 { lib, pkgs, ... }:
 lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-  home.packages = [ pkgs.zoom-us ];
+  home.packages = [
+    (pkgs.zoom-us.override {
+      # Zoom runs in an FHS environment. Include the portals there so its
+      # Wayland/PipeWire screen-sharing integration can discover them.
+      gnomeXdgDesktopPortalSupport = true;
+    })
+  ];
 }
