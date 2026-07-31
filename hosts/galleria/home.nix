480a1c319485d0686400063fed382cea823bc66c
moons <moons@moons14.com>
2026-07-31 13:40:16 +0900

    display

---
+++ hosts/galleria/home.nix
@@ -0,0 +1,16 @@
+{
+  programs.niri.settings.outputs = {
+    "HDMI-A-1" = {
+      scale = 1.5;
+      position = {
+        x = 0;
+        y = 0;
+      };
+    };
+
+    "DP-1".position = {
+      x = 2560;
+      y = 0;
+    };
+  };
+}
