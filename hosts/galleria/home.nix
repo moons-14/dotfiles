c3dbcda528e2671744df4f84753ff70f23e99fb1
moons <moons@moons14.com>
2026-07-31 13:58:09 +0900

    display

--- hosts/galleria/home.nix
+++ hosts/galleria/home.nix
@@ -1,4 +1,35 @@
 {
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "labwc-session.target";
+    settings = [
+      {
+        profile = {
+          name = "galleria";
+          outputs = [
+            {
+              criteria = "HDMI-A-1";
+              status = "enable";
+              position = "0,0";
+              scale = 1.5;
+            }
+            {
+              criteria = "DP-1";
+              status = "enable";
+              position = "2560,0";
+            }
+            {
+              criteria = "DP-2";
+              status = "enable";
+              position = "5120,0";
+              scale = 1.5;
+            }
+          ];
+        };
+      }
+    ];
+  };
+
   programs.niri.settings.outputs = {
     "HDMI-A-1" = {
       scale = 1.5;
@@ -11,6 +42,14 @@
     "DP-1".position = {
       x = 2560;
       y = 0;
+    };
+
+    "DP-2" = {
+      scale = 1.5;
+      position = {
+        x = 5120;
+        y = 0;
+      };
     };
   };
 }
