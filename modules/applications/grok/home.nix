ac874a849ea54c4ea814454fbe1eebbdbf000e77
moons <moons@moons14.com>
2026-08-02 22:06:39 +0900

    grok

--- modules/applications/grok/home.nix
+++ modules/applications/grok/home.nix
@@ -1,9 +1,6 @@
 { inputs, pkgs, ... }:
-let
-  grok = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.grok.overrideAttrs (_: {
-    versionCheckProgram = "${placeholder "out"}/libexec/grok/grok-launcher";
-  });
-in
 {
-  home.packages = [ grok ];
+  home.packages = [
+    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.grok
+  ];
 }
