847d7ee6dd154b7dd9b00e9693186acf88098d2f
moons <moons@moons14.com>
2026-08-02 21:48:38 +0900

    nix-index

--- modules/applications/nix-index/home.nix
+++ modules/applications/nix-index/home.nix
@@ -1,4 +1,14 @@
 {
-  programs.nix-index.enable = true;
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  programs.nix-index = {
+    enable = true;
+
+    package =
+      inputs.nix-index-database.packages.${pkgs.stdenv.hostPlatform.system}.nix-index-with-small-db;
+  };
   programs.nix-index-database.comma.enable = true;
 }
