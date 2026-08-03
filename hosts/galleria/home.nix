49141e3478fdd6d00a7ad2881e0e5db228030c1a
moons <moons@moons14.com>
2026-08-03 21:45:35 +0900

    kanshi

--- hosts/galleria/home.nix
+++ hosts/galleria/home.nix
@@ -1,11 +1,12 @@
 {
   services.kanshi = {
     enable = true;
-    systemdTarget = "labwc-session.target";
+
     settings = [
       {
         profile = {
           name = "galleria";
+
           outputs = [
             {
               criteria = "HDMI-A-1";
@@ -28,28 +29,5 @@
         };
       }
     ];
-  };
-
-  programs.niri.settings.outputs = {
-    "HDMI-A-1" = {
-      scale = 1.5;
-      position = {
-        x = 0;
-        y = 0;
-      };
-    };
-
-    "DP-1".position = {
-      x = 2560;
-      y = 0;
-    };
-
-    "DP-2" = {
-      scale = 1.5;
-      position = {
-        x = 5120;
-        y = 0;
-      };
-    };
   };
 }
--- hosts/x1g13/home.nix
+++ hosts/x1g13/home.nix
@@ -1,12 +1,12 @@
 {
-
   services.kanshi = {
     enable = true;
-    systemdTarget = "labwc-session.target";
+
     settings = [
       {
         profile = {
           name = "x1g13";
+
           outputs = [
             {
               criteria = "eDP-1";
@@ -18,13 +18,5 @@
         };
       }
     ];
-  };
-
-  programs.niri.settings.outputs."eDP-1" = {
-    scale = 1.5;
-    position = {
-      x = 0;
-      y = 0;
-    };
   };
 }
