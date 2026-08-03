e708ceee26a799b99e67c0db7ed3b47d593ecc09
moons <moons@moons14.com>
2026-08-03 14:32:59 +0900

    nh

--- modules/applications/nh/home.nix
+++ modules/applications/nh/home.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 {
   home.packages = [ pkgs.nh ];
-  home.sessionVariables.NH_FLAKE = "${config.home.homeDirectory}/dotfiles";
+  home.sessionVariables = {
+    NH_FLAKE = "${config.home.homeDirectory}/dotfiles";
+    NH_SHOW_ACTIVATION_LOGS = "1";
+  };
 }
