98397cf1529b30af2fd7e344b6cdd6b226e7ef53
moons <moons@moons14.com>
2026-08-03 15:11:58 +0900

    polkit-gnome

--- modules/services/polkit-gnome/home.nix
+++ modules/services/polkit-gnome/home.nix
@@ -1,32 +1,3 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  mkSessionService = target: {
-    Unit = {
-      Description = "GNOME PolicyKit authentication agent";
-      Documentation = [ "man:polkit(8)" ];
-      PartOf = [ target ];
-      After = [ target ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      Restart = "on-failure";
-    };
-
-    Install.WantedBy = [ target ];
-  };
-in
-{
-  systemd.user.services =
-    lib.optionalAttrs config.my.applications.niri.enable {
-      polkit-gnome-niri = mkSessionService "niri.service";
-    }
-    // lib.optionalAttrs config.my.applications.labwc.enable {
-      polkit-gnome-labwc = mkSessionService "labwc-session.target";
-    };
+_: {
+  services.polkit-gnome.enable = true;
 }
