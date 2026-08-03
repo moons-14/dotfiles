5edbc6cbb4c6098fd193c1413233fd2ba43f690b
moons <moons@moons14.com>
2026-08-03 14:50:42 +0900

    zsh

--- modules/applications/zsh/home.nix
+++ modules/applications/zsh/home.nix
@@ -1,7 +1,17 @@
 {
+  home.shellAliases = {
+    "docker-compose" = "docker compose";
+
+    lssh =
+      "ssh"
+      + " -o KexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1"
+      + " -o Ciphers=+aes256-cbc,aes128-cbc"
+      + " -o HostKeyAlgorithms=+ssh-rsa"
+      + " -o SetEnv=TERM=xterm";
+  };
+
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
 
     oh-my-zsh = {
       enable = true;
@@ -12,28 +22,23 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    initContent = ''
-      export HISTSIZE=999999999
-      export HISTFILESIZE=999999999
+    history = {
+      size = 999999999;
+      save = 999999999;
 
-      setopt extended_history
-      setopt hist_allow_clobber
-      setopt hist_fcntl_lock
-      setopt hist_find_no_dups
-      setopt hist_ignore_all_dups
-      setopt hist_ignore_dups
-      setopt hist_ignore_space
-      setopt hist_reduce_blanks
-      setopt hist_save_no_dups
-      setopt hist_verify
-      setopt inc_append_history_time
+      append = true;
+      extended = true;
+      findNoDups = true;
+      ignoreAllDups = true;
+      saveNoDups = true;
+      share = false;
+    };
 
-      unsetopt SHARE_HISTORY
-      setopt APPEND_HISTORY
-      setopt INC_APPEND_HISTORY_TIME
-
-      alias docker-compose="docker compose"
-      alias lssh='ssh -o KexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1 -o Ciphers=+aes256-cbc,aes128-cbc -o HostKeyAlgorithms=+ssh-rsa -o SetEnv=TERM=xterm'
-    '';
+    setOptions = [
+      "HIST_ALLOW_CLOBBER"
+      "HIST_REDUCE_BLANKS"
+      "HIST_VERIFY"
+      "INC_APPEND_HISTORY_TIME"
+    ];
   };
 }
